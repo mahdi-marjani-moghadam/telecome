9622f65feae1cab0c1aafb538ed992c2288791e4
nazanin <vosoughi.nazanin@gmail.com>
2021-09-18 14:22:40 +0200

    first commit

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -1,5 +1,15 @@
 @extends('app')
 
 @section('content')
-    about
+
+<div class="container align-items-stretch p-5 ">
+    <div class="row align-items-center">
+    <div class="card ">
+        <div class="card-body p-4">
+            <h4 class="card-title">Über uns</h4>
+            <p class="card-text text-wrap">Telecome Mobile ist der führende deutsche Anbieter von Kommunikation für Menschen mit Migrationshintergrund.Von Düsseldorf aus bieten wir Mobilfunkprodukte auf Prepaid-Basis für attraktive Verbindungen ins europäische und internationale Ausland, sowie innerhalb Deutschlands an. Zum preiswerten und transparenten Basis-Tarif können verschiedene Optionen hinzu gebucht werden, beispielsweise für die Nutzung von SMS, Roaming oder dem mobilen Internet.</p>
+        </div>
+    </div>
+</div>
+</div>
 @endsection
