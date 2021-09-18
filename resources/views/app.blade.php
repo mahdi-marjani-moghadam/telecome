d50c3d14215665f001497eaa0db3ebbf2d40b3fb
nazanin <vosoughi.nazanin@gmail.com>
2021-09-18 14:00:44 +0200

    first commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -18,7 +18,7 @@
 
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container">
-                <img src="{{ asset('\images\logo.png') }}" class="logo"/>
+                <img src="{{ asset('\images\logo.png') }}" class="logo" alt="">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
